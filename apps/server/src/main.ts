d58386948c02e71baf0e82c702a27f00e97f64f2
42KIM <ksahoo524@gmail.com>
2025-05-13 21:33:34 +0900

    nest swagger 적용

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -19,6 +20,18 @@ async function bootstrap() {
     new ClassSerializerInterceptor(app.get('Reflector')),
   );
 
+  const config = new DocumentBuilder()
+    .setTitle('Project Daepa API')
+    .setDescription('Project Daepa API description')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, documentFactory, {
+    raw: ['json'],
+    jsonDocumentUrl: '/api-docs/json',
+  });
+
   await app.listen(process.env.PORT ?? 4000);
 }
+
 bootstrap();
