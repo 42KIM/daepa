1ecdc5604e372eaa7de2b60c7e0146ba4c10b17f
42KIM <ksahoo524@gmail.com>
2025-05-19 21:56:00 +0900

    targetId 필드를 Nullable하게 수정

--- apps/server/src/user_notification/user_notification.dto.ts
+++ apps/server/src/user_notification/user_notification.dto.ts
@@ -46,7 +46,8 @@ class UserNotificationDto {
     example: 'XXXXXXXX',
   })
   @IsString()
-  targetId: string;
+  @IsOptional()
+  targetId?: string;
 
   @ApiProperty({
     description: '알림 상태',
