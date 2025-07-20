e10c015f31523ba286beb361ba5563ff173e69e5
42KIM <ksahoo524@gmail.com>
2025-07-20 22:44:39 +0900

    동시성 문제 방지 로직 추가

--- apps/server/src/laying/laying.service.ts
+++ apps/server/src/laying/laying.service.ts
@@ -3,6 +3,7 @@ import { LayingEntity } from './laying.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateLayingDto } from './laying.dto';
+import { isMySQLError } from 'src/common/error';
 
 @Injectable()
 export class LayingService {
@@ -12,17 +13,35 @@ export class LayingService {
   ) {}
 
   async createLaying(createLayingDto: CreateLayingDto) {
-    const clutchEggs = await this.layingRepository.countBy({
-      matingId: createLayingDto.matingId,
-      layingDate: createLayingDto.layingDate,
-    });
+    let retries = 5;
 
-    const incrementedLayingOrder = clutchEggs + 1;
+    // incrementedLayingOrder 생성 시의 동시성 문제 해결 목적
+    while (retries > 0) {
+      try {
+        const clutchEggs = await this.layingRepository.countBy({
+          matingId: createLayingDto.matingId,
+          layingDate: createLayingDto.layingDate,
+        });
 
-    const layingEntity = this.layingRepository.create({
-      ...createLayingDto,
-      layingOrder: incrementedLayingOrder,
-    });
-    return await this.layingRepository.save(layingEntity);
+        const incrementedLayingOrder = clutchEggs + 1;
+
+        const layingEntity = this.layingRepository.create({
+          ...createLayingDto,
+          layingOrder: incrementedLayingOrder,
+        });
+        return await this.layingRepository.save(layingEntity);
+      } catch (error) {
+        if (
+          isMySQLError(error) &&
+          error.code === 'ER_DUP_ENTRY' &&
+          retries > 1
+        ) {
+          retries--;
+          await new Promise((resolve) => setTimeout(resolve, 100));
+          continue;
+        }
+        throw error;
+      }
+    }
   }
 }
