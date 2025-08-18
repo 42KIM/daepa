71543d99cceb2fb5f4e206a590ec0e97811bf137
정예원 <yes3427@kookmin.ac.kr>
2025-08-18 16:49:30 +0900

    몹라일 카카오톡 로그인 기능 추가

--- apps/mobile/src/screens/Settings/index.tsx
+++ apps/mobile/src/screens/Settings/index.tsx
@@ -1,10 +1,69 @@
-import { View, Text } from 'react-native';
+import { Button } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { login, getProfile } from '@react-native-seoul/kakao-login';
+import {
+  authControllerGetToken,
+  authControllerKakaoNative,
+} from '../../../../../packages/api-client/src/api';
+import { setupApiClient } from '../../utils/apiSetup';
+import { useMutation } from '@tanstack/react-query';
+import { UserDtoStatus } from '@repo/api-client';
+import { useNavigation } from '@react-navigation/native';
+import { tokenStorage } from '../../utils/tokenStorage';
+import Profile from './Profile';
 
 const SettingsScreen = () => {
+  const navigation = useNavigation();
+
+  const { mutate: mutateGetToken } = useMutation({
+    mutationFn: async (_status: UserDtoStatus) => {
+      return authControllerGetToken();
+    },
+    onSuccess: (data, status) => {
+      switch (status) {
+        case UserDtoStatus.PENDING:
+          tokenStorage.setToken(data.data.token);
+          navigation.navigate('Register');
+          break;
+        case UserDtoStatus.ACTIVE:
+          tokenStorage.setToken(data.data.token);
+          navigation.navigate('Home');
+          break;
+        default:
+          navigation.navigate('Settings');
+          break;
+      }
+    },
+  });
+
+  const { mutate: kakaoNativeLogin } = useMutation({
+    mutationFn: authControllerKakaoNative,
+    onSuccess: data => {
+      mutateGetToken(data.data.status);
+    },
+    onError: error => {
+      console.log(error);
+    },
+  });
+  const handleKakaoLogin = async () => {
+    try {
+      setupApiClient();
+      const kakaoLogin = await login();
+      const userInfo = await getProfile();
+
+      kakaoNativeLogin({
+        email: userInfo.email ?? '',
+        id: String(userInfo.id),
+        refreshToken: kakaoLogin.refreshToken,
+      });
+    } catch (e: any) {}
+  };
+
   return (
-    <View>
-      <Text>Settings</Text>
-    </View>
+    <SafeAreaView>
+      <Profile />
+      <Button title="카카오톡 로그인" onPress={handleKakaoLogin} />
+    </SafeAreaView>
   );
 };
 
