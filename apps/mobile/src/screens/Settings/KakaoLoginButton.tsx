a91dbb73fb0245ffbeb503708b6007953251c807
정예원 <yes3427@kookmin.ac.kr>
2025-08-20 14:07:23 +0900

    카카오 로그인 버튼 컴포넌트 분리

---
+++ apps/mobile/src/screens/Settings/KakaoLoginButton.tsx
@@ -0,0 +1,65 @@
+import { Button } from 'react-native';
+import { login, getProfile } from '@react-native-seoul/kakao-login';
+import {
+  authControllerGetToken,
+  authControllerKakaoNative,
+} from '../../../../../packages/api-client/src/api';
+import { setupApiClient } from '../../utils/apiSetup';
+import { useMutation } from '@tanstack/react-query';
+import { UserDtoStatus } from '@repo/api-client';
+import useLogin from '../../hooks/useLogin';
+import Loading from '@/components/common/Loading';
+import Toast from '@/components/common/Toast';
+
+const KakaoLoginButton = () => {
+  const { navigateByStatus } = useLogin();
+
+  const { mutate: mutateGetToken } = useMutation({
+    mutationFn: async (_status: UserDtoStatus) => {
+      return authControllerGetToken();
+    },
+    onSuccess: async (data, status) => {
+      navigateByStatus({ status, token: data.data.token });
+
+      Loading.close();
+      Toast.show('로그인에 성공했습니다.');
+    },
+    onError: () => {
+      Loading.close();
+      Toast.show('로그인에 실패했습니다. 다시 시도해주세요.');
+    },
+  });
+
+  const { mutate: kakaoNativeLogin } = useMutation({
+    mutationFn: authControllerKakaoNative,
+    onSuccess: data => {
+      mutateGetToken(data.data.status);
+    },
+    onError: error => {
+      console.log(error);
+      Loading.close();
+    },
+  });
+
+  const handleKakaoLogin = async () => {
+    Loading.show();
+    try {
+      setupApiClient();
+      const kakaoLogin = await login();
+      const userInfo = await getProfile();
+
+      kakaoNativeLogin({
+        email: userInfo.email ?? '',
+        id: String(userInfo.id),
+        refreshToken: kakaoLogin.refreshToken,
+      });
+    } catch (e: any) {
+      console.log(e);
+      Loading.close();
+    }
+  };
+
+  return <Button title="카카오톡 로그인" onPress={handleKakaoLogin} />;
+};
+
+export default KakaoLoginButton;
