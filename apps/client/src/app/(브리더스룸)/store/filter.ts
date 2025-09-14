8397220c90af70420aa892346d9b551e84d3c4a1
42KIM <ksahoo524@gmail.com>
2025-09-14 22:50:35 +0900

    searchFilter 타입 수정

--- apps/client/src/app/(브리더스룸)/store/filter.ts
+++ apps/client/src/app/(브리더스룸)/store/filter.ts
@@ -2,23 +2,21 @@ import { create } from "zustand";
 import { PetControllerFindAllParams, PetDto } from "@repo/api-client";
 import { VisibilityState } from "@tanstack/react-table";
 
-export interface FilterStore<P extends object, C extends string = string> {
-  searchFilters: Partial<P>;
+export interface FilterStore<PetControllerFindAllParams> {
+  searchFilters: Partial<PetControllerFindAllParams>;
   columnFilters?: VisibilityState;
-  setSearchFilters: (filters: Partial<P>) => void;
-  setColumnFilters: (columnFilters: Partial<Record<C, boolean>>) => void;
+  setSearchFilters: (filters: Partial<PetControllerFindAllParams>) => void;
+  setColumnFilters: (columnFilters: Partial<VisibilityState>) => void;
 }
 
-export const useFilterStore = create<FilterStore<PetControllerFindAllParams, keyof PetDto>>()(
-  (set) => ({
-    searchFilters: {},
-    columnFilters: undefined,
+export const useFilterStore = create<FilterStore<PetControllerFindAllParams>>()((set) => ({
+  searchFilters: {},
+  columnFilters: undefined,
 
-    // Actions
-    setSearchFilters: (filters) => set({ searchFilters: filters }),
-    setColumnFilters: (newFilter: Partial<Record<keyof PetDto, boolean>>) =>
-      set((state) => ({
-        columnFilters: state.columnFilters ? { ...state.columnFilters, ...newFilter } : newFilter,
-      })),
-  }),
-);
+  // Actions
+  setSearchFilters: (filters) => set({ searchFilters: filters }),
+  setColumnFilters: (newFilter: Partial<Record<keyof PetDto, boolean>>) =>
+    set((state) => ({
+      columnFilters: state.columnFilters ? { ...state.columnFilters, ...newFilter } : newFilter,
+    })),
+}));
